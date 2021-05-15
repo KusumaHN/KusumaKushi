077bb35bc47027f1d0529fd80031786d4b91ea1d
Kusumahn23@gmail.com <Kusumahn23@gmail.com>
2021-05-15 13:27:40 +0530

    Second

---
+++ KusumaCode/Fact.java
@@ -0,0 +1,17 @@
+package KusumaCode;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int fact=1;
+		
+		for(int i=1; i<=6; i++)
+		{
+			fact=fact*i;
+		}
+System.out.println(fact);
+	}
+
+}
